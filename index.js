aa9881da389ed512f6a28733660f11835835d648
Cesar Diaz <dev.cdiaz@gmail.com>
2023-07-25 09:44:28 -0500

    error messages added + tailwind corrections

--- index.js
+++ index.js
@@ -20,40 +20,60 @@ let cardImageName = document.getElementById(`namePlaceholder`);
 let cardImageNumber = document.getElementById(`numberPlaceholder`);
 let cardImageDates = document.getElementById(`expirationPlaceholder`);
 
-// Clearing the input fields and changing the input text to black
+// Declaring the errors
+let nameError = document.getElementById(`invalidName`);
+let cardError = document.getElementById(`invalidCard`);
+let dateError = document.getElementById(`invalidDate`);
+let cvcError = document.getElementById(`invalidCvc`);
+
+// Clearing the input fields and changing the input text to black, will only clear them initially and will not clear the user's input if they have to click in it again to correct something
 
 cardholderInput.addEventListener(`click`, function () {
-  cardholderInput.value = ``;
+  if (cardholderInput.value == `e.g. Jane Appleseed`) {
+    cardholderInput.value = ``;
+  }
   cardholderInput.style.color = `black`;
 });
 cardNumberInput.addEventListener(`click`, function () {
-  cardNumberInput.value = ``;
+  if (cardNumberInput.value == `e.g. 1234 5678 9123 0000`) {
+    cardNumberInput.value = ``;
+  }
   cardNumberInput.style.color = `black`;
 });
 expInputM.addEventListener(`click`, function () {
-  expInputM.value = ``;
+  if (expInputM.value == `MM`) {
+    expInputM.value = ``;
+  }
   expInputM.style.color = `black`;
 });
 expInputY.addEventListener(`click`, function () {
-  expInputY.value = ``;
+  if (expInputY.value == `YYYY`) {
+    expInputY.value = ``;
+  }
   expInputY.style.color = `black`;
 });
 cvcInput.addEventListener(`click`, function () {
-  cvcInput.value = ``;
+  if (cvcInput.value == `e.g. 123`) {
+    cvcInput.value = ``;
+  }
   cvcInput.style.color = `black`;
 });
 
 // function for checking valid cardholder name Input
 function cardHolderNameCheck() {
   const cardholderValue = cardholderInput.value.trim(); // Remove leading and trailing spaces
-
+  let nameErrorMessage = (nameError.style.display = `block`);
   if (cardholderValue.length <= 2) {
+    nameErrorMessage;
     return false;
   } else if (!/^[a-zA-Z\s]+$/.test(cardholderValue)) {
+    nameErrorMessage;
     return false;
   } else if (cardholderValue === null || cardholderValue === undefined) {
+    nameErrorMessage;
     return false;
   } else {
+    nameError.style.display = `none`;
     return true;
   }
 }
@@ -61,12 +81,15 @@ function cardHolderNameCheck() {
 // function for checking valid card number
 function cardNumberCheck() {
   const cardNumber = cardNumberInput.value;
-
+  let cardErrorMessage = (cardError.style.display = `block`);
   if (isNaN(cardNumber)) {
+    cardErrorMessage;
     return false;
   } else if (cardNumber.length < 16 || cardNumber.length > 16) {
+    cardErrorMessage;
     return false;
   } else {
+    cardError.style.display = `none`;
     return true;
   }
 }
@@ -97,11 +120,15 @@ function checkYear() {
 }
 
 function checkDates() {
+  let dateErrorMessage = (invalidDate.style.display = `block`);
   if (checkMonth() != true) {
+    dateErrorMessage;
     return false;
   } else if (checkYear() != true) {
+    dateErrorMessage;
     return false;
   } else {
+    invalidDate.style.display = `none`;
     return true;
   }
 }
@@ -110,12 +137,15 @@ function checkDates() {
 
 function cvcCheck() {
   const cvc = cvcInput.value;
-
+  let cvcErrorMessage = (cvcError.style.display = `block`);
   if (isNaN(cvc)) {
+    cvcErrorMessage;
     return false;
   } else if (cvc.length > 3 || cvc.length < 3) {
+    cvcErrorMessage;
     return false;
   } else {
+    cvcError.style.display = `none`;
     return true;
   }
 }
@@ -140,6 +170,6 @@ confirmBtn.addEventListener(`click`, function () {
 
 // Confirm reload  ?
 
-window.onbeforeunload = function () {
-  return `Are you sure you want to leave?`;
-};
+// window.onbeforeunload = function () {
+//   return `Are you sure you want to leave?`;
+// };
